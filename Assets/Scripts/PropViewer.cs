ae421b2d76fa8804ac040e717996978c5b51138d
TheAvaroo <55988186+TheAvaro@users.noreply.github.com>
2020-02-21 19:40:55 +0100

    cosas2

--- Assets/Scripts/PropViewer.cs
+++ Assets/Scripts/PropViewer.cs
@@ -6,8 +6,27 @@ public class PropViewer : MonoBehaviour
 {
     public GameObject[] props;
 
-    void Update()
-	{
+	int selection;
 
+	void Start()
+	{
+		selection = 0;
 	}
+	/*
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			if(selection < props.size() - 1);
+				selection++;
+		}
+		if(Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			if(selection > 0)
+			{
+				selection--;
+			}
+		}
+	}
+	*/
 }
