0f95e45d24d503648d1baba29de0afdb56edd3b9
Lajaraa <albertlajara2018@cev.com>
2020-04-24 21:59:03 +0200

    botones y puerta

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public GameObject door;
+	public int numBoton;
+	AudioSource tuit;
+
+	DoorScript ds;
+
+	private void Start()
+	{
+		ds = door.GetComponent<DoorScript>();
+		tuit = this.GetComponent<AudioSource>();
+	}
+
+	private void OnTriggerEnter(Collider collision)
+	{
+		if (collision.tag == "Player")
+		{
+			ds.SetBut(numBoton, true);
+			tuit.Play();
+			this.gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public bool[] botones = { false, false };
+    GameObject door;
+
+    private void Start()
+    {
+        door = this.gameObject;
+    }
+
+    public void SetBut(int numBot, bool on)
+    {
+        botones[numBot] = on;
+        bool abre = true;
+        for (int i = 0; i<botones.Length; ++i)
+        {
+            if (botones[i] == false) abre = false;
+        }
+        if (abre == true) door.SetActive(false);
+        else door.SetActive(true);
+    }
+}
